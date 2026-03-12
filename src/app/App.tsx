03bcaad75ddf4d5863ebba8a0585fa1dd061b0cc
João Victor Ventura Martins <jvmartinscv@gmail.com>
2026-03-12 09:21:47 -0300

    chore(project): criar estrutura inicial do projeto

---
+++ src/app/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-black">
+      <h1 className="text-4xl text-white font-bold">Align3D</h1>
+    </div>
+  );
+}
